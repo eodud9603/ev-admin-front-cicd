69d77601996d1425d97bd61916eca61f6c16ea69
홍준택 <jthong@humaxmobility.co.kr>
2023-03-06 17:07:21 +0900

    feat: radio 컴포넌트 추가
    공통 radio 컴포넌트를 추가하였습니다.
    
    - radio 디자인 적용

---
+++ src/components/Common/Radio/RadioBase.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { FormGroup, Input, Label } from "reactstrap";
+
+interface IRadioBaseProps {
+  name: string;
+  label: string;
+
+  id?: string;
+  disabled?: boolean;
+  checked?: boolean;
+  value?: string;
+  onChange?: React.ChangeEventHandler<HTMLInputElement>;
+}
+
+const RadioBase = (props: IRadioBaseProps) => {
+  const {
+    /* required */
+    name = "radio",
+    label = "label 텍스트",
+    /* optional */
+    id,
+    disabled,
+    checked,
+    value,
+    onChange,
+    /* rest */
+    ...rest
+  } = props;
+
+  return (
+    <FormGroup className={"form-check-turu"} check inline disabled={disabled}>
+      <Input
+        id={id}
+        type={"radio"}
+        name={name}
+        disabled={disabled}
+        checked={checked}
+        value={value}
+        onChange={onChange}
+        {...rest}
+      />
+      <Label check className={"text-nowrap"} htmlFor={id} disabled={disabled}>
+        {label}
+      </Label>
+    </FormGroup>
+  );
+};
+
+export default RadioBase;
