0a26adb3d96813bd64625b68cfd524d2126fb989
김대영 <eodud9603@gmail.com>
2023-03-06 20:42:42 +0900

    feat: colors add

---
+++ src/assets/colors.ts
@@ -0,0 +1,13 @@
+const Colors = {
+  BLUE: "#5156be", //primary
+  TURU: "#FC6C00", //turu
+  GRAY: "#74788d", //secondary
+  GREEN: "#2ab57d", //success
+  CYAN: "#4ba6ef", //info
+  YELLOW: "#ffbf53", //warning
+  RED: "#fd625e", //danger
+  PINK: "#e83e8c", //pink
+  DARK: "#2c302e", //dark
+  LIGHTGRAY: "#e9e9ef", //light
+};
+export default Colors;
