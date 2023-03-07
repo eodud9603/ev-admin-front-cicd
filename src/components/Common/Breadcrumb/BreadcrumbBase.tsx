b42274d6f0e65ad888a2b6206d9695ca633a4024
홍준택 <jthong@humaxmobility.co.kr>
2023-03-07 18:39:27 +0900

    design: Breadcrumb 컴포넌트 수정
    Breadcrumb 컴포넌트 수정하였습니다.
    
    - Breadcrumb > title 추가

--- src/components/Common/Breadcrumb/BreadcrumbBase.tsx
+++ src/components/Common/Breadcrumb/BreadcrumbBase.tsx
@@ -4,12 +4,14 @@ import styled from "styled-components";
 
 interface IBreadcrumbProps {
   list: { label: string; href: string }[];
+  title?: string;
 }
 
 const BreadcrumbBase = (props: IBreadcrumbProps) => {
   const {
     /* required */
     list = [],
+    title,
     /* optional */
     /* rest */
   } = props;
@@ -23,26 +25,29 @@ const BreadcrumbBase = (props: IBreadcrumbProps) => {
   };
 
   return (
-    <Breadcrumb
-      tag={"nav"}
-      listTag={"div"}
-      listClassName={"m-0 mb-2 p-0"}
-      className={"relay-breadcrumb"}
-    >
-      {list.map(({ label, href }, index) => (
-        <Item
-          key={`${label}-${href}`}
-          tag={"a"}
-          href={href}
-          className={`text-dark font-size-14 left-0 fw-${
-            lastIndexCheck(index) ? "bold" : "normal"
-          }`}
-          active={!lastIndexCheck(index)}
-        >
-          {label}
-        </Item>
-      ))}
-    </Breadcrumb>
+    <>
+      <Breadcrumb
+        tag={"nav"}
+        listTag={"div"}
+        listClassName={"m-0 mb-2 p-0"}
+        className={"relay-breadcrumb"}
+      >
+        {list.map(({ label, href }, index) => (
+          <Item
+            key={`${label}-${href}`}
+            tag={"a"}
+            href={href}
+            className={`text-dark font-size-14 left-0 fw-${
+              lastIndexCheck(index) ? "bold" : "normal"
+            }`}
+            active={!lastIndexCheck(index)}
+          >
+            {label}
+          </Item>
+        ))}
+      </Breadcrumb>
+      {Boolean(title) && <h3 className={"font-size-24 mb-4"}>{title}</h3>}
+    </>
   );
 };
 
