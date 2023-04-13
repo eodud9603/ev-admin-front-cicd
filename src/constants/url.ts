807db3dc77b2badb36521f06fc0ad6cf4ed4b4b3
홍준택 <jthong@humaxmobility.co.kr>
2023-04-13 13:42:46 +0900

    fix: 서버 url 주소 변경

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,5 +1,5 @@
 export const API_URL = {
-  baseUrl: "http://218.38.12.31:45081/v1",
+  baseUrl: "http://218.38.12.198:45081/v1",
   authUrl: "/authenticate",
   regionUrl: "/members/search/region",
   stationUrl: "/station",
