7aabad5501d42c182f7daa1ad181afb1118b6bb8
홍준택 <jthong@humaxmobility.co.kr>
2023-05-19 15:48:26 +0900

    feat: 충전소 및 충전기 관리 > 충전기 고장/파손 관리 > 목록 조회 loaderData 로직 수정

--- src/pages/Charger/loader/brokenListLoader.ts
+++ src/pages/Charger/loader/brokenListLoader.ts
@@ -3,38 +3,59 @@ import { IRequestBrokenList } from "src/api/broken/brokenApi.interface";
 import { OperatorType } from "src/api/api.interface";
 import { TChargerProcessingStatus } from "src/constants/status";
 import { loadTabData } from "src/utils/loadTabData";
-
-const defaultParams: IRequestBrokenList = {
-  /** @TODO 서버 sortDirection 정의 후, 추가 */
-  // sortDirection: "ASC",
-  size: 10,
-  page: 0,
-  sort: "CreatedDate",
-};
+import { getParams } from "src/utils/params";
+import { standardDateFormat } from "src/utils/day";
 
 export const INIT_TROUBLE = {
+  size: "10",
+  page: 0,
   sido: "",
   gugun: "",
   dong: "",
-  searchRange: "StationName",
-  startDate: "",
-  endDate: "",
-  searchText: "",
+  searchType: "StationName",
+  submitStartDate: "",
+  submitEndDate: "",
+  searchKeyword: "",
   operator: "" as OperatorType,
   sort: "CreatedDate",
   status: "" as TChargerProcessingStatus,
-  count: "10",
 };
 
 export const brokenListLoader = async () => {
   const loadData = loadTabData("/charger/trouble");
-  if (Object.keys(loadData?.data ?? {}).length > 0) {
-    return loadData;
+
+  const params = {
+    ...INIT_TROUBLE,
+    ...(loadData?.filterData as unknown as IRequestBrokenList),
+    page: (loadData?.currentPage ?? 1) - 1,
+  } as IRequestBrokenList;
+
+  getParams(params);
+  if (!params.searchKeyword) {
+    delete params.searchType;
   }
+  if (!params.submitStartDate || !params.submitEndDate) {
+    delete params.submitStartDate;
+    delete params.submitEndDate;
+  } else {
+    params.submitStartDate = standardDateFormat(
+      params.submitStartDate,
+      "YYYY.MM.DD"
+    );
+    params.submitEndDate = standardDateFormat(
+      params.submitEndDate,
+      "YYYY.MM.DD"
+    );
+  }
+
   /* 검색  */
-  const { code, data } = await getBrokenList(defaultParams);
+  const { code, data } = await getBrokenList(params);
   /** 검색 성공 */
   const success = code === "SUCCESS" && !!data;
 
-  return { data: success ? data : {}, filterData: INIT_TROUBLE };
+  return {
+    data: success ? data : {},
+    filterData: loadData?.filterData ?? INIT_TROUBLE,
+    currentPage: loadData?.currentPage,
+  };
 };
